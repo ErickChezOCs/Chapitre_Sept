30003b466d8b24446a186f80eed5c0500d3a7cd2
ErickChezOCs <nounga72@yahoo.fr>
2023-04-16 17:40:05 +0200

    test_z

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductsModule } from './products/products.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     ProductsModule,
+    AppRoutingModule,
+    HttpClientModule,
     AuthModule
   ],
   providers: [],
